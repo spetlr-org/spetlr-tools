fadc774ca88588d7b395e82901500dff009c0827
Lau Johansson <ljo@delegate.dk>
2024-01-19 15:49:49 +0100

    fix: cleanup and improve code description

--- src/spetlrtools/cli_install/defaults.py
+++ src/spetlrtools/cli_install/defaults.py
@@ -1,3 +1,15 @@
+"""
+This script is designed for setting up environment-specific configurations for the Databricks CLI tool.
+It contains functionality to:
+- Detect the operating system of the user and determine if it is Windows.
+- Define default target paths for the Databricks CLI executable based on the operating system.
+- Store the GitHub repository URL of the Databricks CLI for reference or further use.
+
+The script uses the 'platform' and 'sysconfig' modules to accurately identify the environment and
+set file paths, ensuring compatibility with different operating systems, particularly Windows and Unix-based systems.
+"""
+
+
 import platform
 import re
 import sysconfig
