06a946c68369063ee264259cfccbf0892caf60ba
Simon Heisterkamp <exshe@clever.dk>
2025-07-01 10:42:55 +0200

    allow fetch after re-run

--- src/spetlrtools/test_job/RunDetails.py
+++ src/spetlrtools/test_job/RunDetails.py
@@ -28,7 +28,13 @@ class RunDetails:
 
         print(f"Getting stdout for {task_key}")
         task: jobs.RunTask
-        (task,) = [t for t in self.details.tasks if t.task_key == task_key]
+
+        # get latest attempt for all tasks of this key
+        # this allows fetch to succeed in case a "repair" fixed a transient task failure
+        tasks_for_key = (t for t in self.details.tasks if t.task_key == task_key)
+        # sort by negative attempt number
+        task = sorted(tasks_for_key,key=lambda t:-t.attempt_number)[0]
+
         task_id = task.run_id
         output = self._db.get_run_output(task_id)
         return output.logs
